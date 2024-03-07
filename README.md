f5801cba95175e00f10d2bfd41be60b4d34fe5ef
amar1101 <nakayumi04@gmail.com>
2024-03-08 00:44:42 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Bike Sharing
+
+Ini adalah dataset tentang penyewaan sepeda di tahun 2011 dan 2012.
+
+## Cara Menjalankan Dashboard
+
+Berikut adalah langkah-langkah untuk menjalankan dashboard:
+
+1. **Prasyarat**: Pastikan Anda telah menginstal Python dan semua paket yang diperlukan. Anda dapat menggunakan berkas `requirements.txt` untuk menginstal semua paket yang diperlukan dengan perintah:
+
+    ```
+    pip install -r requirements.txt
+    ```
+
+2. **Menjalankan Aplikasi**: Buka terminal atau command prompt, arahkan ke direktori proyek, dan jalankan perintah berikut:
+
+    ```
+    streamlit run nama_file_dashboard.py
+    ```
+
+    Ganti `nama_file_dashboard.py` dengan nama file Python yang berisi kode untuk dashboard Anda.
+
+3. Setelah itu, dashboard akan dijalankan dan Anda dapat mengaksesnya melalui browser web dengan mengunjungi alamat yang ditampilkan di terminal atau command prompt.
---
+++ dashboard/dashboard.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+def create_monthly_rent_df(df):
+    monthly_rent_df = df.resample(rule='M', on='dteday').size().reset_index(name='jumlah_sepeda_disewa')
+    return monthly_rent_df
+
+def create_byseason_df(df):
+    # Mengubah nilai numerik musim menjadi nama musim
+    season_name = {1: 'Spring', 2: 'Summer', 3: 'Fall', 4: 'Winter'}
+    df['season_name'] = df['season'].map(season_name)
+    
+    # Mengelompokkan berdasarkan nama musim dan menghitung jumlah cnt untuk setiap musim
+    byseason_df = df.groupby('season_name').agg({'cnt': 'sum'}).reset_index()
+    
+    return byseason_df
+
+all_df = pd.read_csv("all_data.csv")
+if 'season_x' in all_df.columns and 'season_y' in all_df.columns:
+    # Jika ada, gabungkan nilai dari kedua kolom menjadi satu kolom
+    all_df['season'] = all_df['season_x'].fillna(all_df['season_y'])
+    # Setelah menggabungkan nilai, Anda bisa menghapus kolom season_x dan season_y
+    all_df.drop(['season_x', 'season_y'], axis=1, inplace=True)
+
+if 'cnt_x' in all_df.columns and 'cnt_y' in all_df.columns:
+    # Jika ada, gabungkan nilai dari kedua kolom menjadi satu kolom
+    all_df['cnt'] = all_df['cnt_x'].fillna(all_df['cnt_y'])
+    # Setelah menggabungkan nilai, Anda bisa menghapus kolom cnt_x dan cnt_y
+    all_df.drop(['cnt_x', 'cnt_y'], axis=1, inplace=True)
+
+
+all_df['dteday'] = pd.to_datetime(all_df['dteday'])
+
+all_df.sort_values(by='dteday', inplace=True)
+all_df.reset_index(inplace=True)
+
+
+min_date = all_df["dteday"].min()
+max_date = all_df["dteday"].max()
+ 
+with st.sidebar:
+
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu', min_value=min_date,
+        max_value=max_date, value=[min_date, max_date]
+    )
+
+main_df = all_df[(all_df["dteday"] >= str(start_date)) & 
+                (all_df["dteday"] <= str(end_date)) & 
+                (all_df["season"].notnull())]
+
+monthly_rent_df = create_monthly_rent_df(main_df)
+byseason_df = create_byseason_df(main_df)
+
+
+st.header('Bike Sharing :sparkles:')
+
+st.subheader('Monthly Rent')
+ 
+col1, col2 = st.columns(2)
+ 
+with col1:
+    total_rent = monthly_rent_df.jumlah_sepeda_disewa.sum()
+    st.metric("Total Rents", value=total_rent)
+ 
+with col2:
+    fig, ax = plt.subplots(figsize=(16, 8))
+    ax.plot(
+        monthly_rent_df["dteday"],
+        monthly_rent_df["jumlah_sepeda_disewa"],
+        marker='o', 
+        linewidth=2,
+        color="#90CAF9"
+    )
+    ax.tick_params(axis='y', labelsize=20)
+    ax.tick_params(axis='x', labelsize=15)
+    st.pyplot(fig)
+
+st.subheader("Customer Demographics by Season")
+
+fig, ax = plt.subplots(figsize=(20, 10))
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+sns.barplot(
+    x="cnt", 
+    y="season_name",
+    data=byseason_df.sort_values(by="cnt", ascending=False),
+    palette=colors,
+    ax=ax
+)
+ax.set_title("Number of Customers by Season", loc="center", fontsize=30)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+st.pyplot(fig)
